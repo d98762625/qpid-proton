ef956f1695963aa5a9563f5fec18272ec73c5b50
d98762625 <d98762625@gmail.com>
2020-03-27 09:54:19 +0000

    remove error example because the output is inconsistent across the travis test environments

--- go/pkg/amqp/url_test.go
+++ go/pkg/amqp/url_test.go
@@ -32,6 +32,7 @@ func ExampleParseURL() {
 		"amqps://host",
 		"/path",
 		"",
+		// Taken out because url.Parse error formatting changed in 1.13
 		":1234",
 		// Taken out because the go 1.4 URL parser isn't the same as later
 		//"[::1]",
@@ -55,5 +56,4 @@ func ExampleParseURL() {
 	// amqps://host:amqps
 	// amqp://localhost:amqp/path
 	// amqp://localhost:amqp
-	// parse ":1234": missing protocol scheme
 }
